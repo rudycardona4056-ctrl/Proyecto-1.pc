a33f244261acacd9b55d4a8e63b8e534316e7c99
rudycardona4056-ctrl <rudycardona4056@gmail.com>
2026-03-18 23:34:29 -0600

    Add files via upload
    Version 1 de proyecto, menu inicial, menu evaluar contenido nuevo: tipo de entretenimiento, la duracion y la clasificacion del usuario.

---
+++ Proyecto 01.RC/Version 1.cs
@@ -0,0 +1,41 @@
+﻿int opcion;
+int tipoentretenimiento;
+int duracion;
+int clasifi;
+
+
+do
+{
+    Console.WriteLine("1. Evaluar contenido nuevo");
+    Console.WriteLine("2. Mostrar reglas del sistema");
+    Console.WriteLine("3. Mostrar estadisticas");
+    Console.WriteLine("4. Reinciar estadisticas");
+    Console.WriteLine("5. Salir");
+    opcion = int.Parse(Console.ReadLine());
+
+    switch (opcion)
+    {
+        case 1:
+            {
+                Console.WriteLine("1. Pelicual");
+                Console.WriteLine("2. Serie");
+                Console.WriteLine("3.Documental");
+                Console.WriteLine("4.Evento en vivo");
+                tipoentretenimiento = int.Parse(Console.ReadLine());
+
+                if (tipoentretenimiento < 1 || tipoentretenimiento > 4)
+                {
+                    Console.WriteLine("Opcion invalida");
+                }
+
+                Console.WriteLine("Duracion en minutos");
+                duracion = int.Parse(Console.ReadLine()); ;
+
+                Console.WriteLine("Clasificacion");
+                Console.WriteLine("1. Todo publico");
+                Console.WriteLine("2. +13");
+                Console.WriteLine("3. +18");
+                clasifi = Console.ReadLine();
+            }
+    }
+}
