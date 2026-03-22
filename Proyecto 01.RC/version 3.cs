d6704b2bf831e9e8dfe053f620f4af05766f4a56
rudycardona4056-ctrl <rudycardona4056@gmail.com>
2026-03-21 23:48:30 -0600

    version 3
    agregue las condiciones siguientes

---
+++ Proyecto 01.RC/version 3.cs
@@ -0,0 +1,94 @@
+﻿int opcion;
+int tipoentretenimiento;
+int duracion;
+int clasifi;
+int ttevaluados = 0;
+int horaprogra;
+int nivelprodu;
+
+
+do
+{
+    Console.WriteLine("1. Evaluar contenido nuevo");
+    Console.WriteLine("2. Mostrar reglas del sistema");
+    Console.WriteLine("3. Mostrar estadisticas");
+    Console.WriteLine("4. Reinciar estadisticas");
+    Console.WriteLine("5. Salir");
+    opcion = int.Parse(Console.ReadLine());
+
+    switch (opcion)
+    {
+        case 1:
+            {
+                Console.WriteLine("1. Pelicual");
+                Console.WriteLine("2. Serie");
+                Console.WriteLine("3.Documental");
+                Console.WriteLine("4.Evento en vivo");
+                tipoentretenimiento = int.Parse(Console.ReadLine());
+
+                if (tipoentretenimiento < 1 || tipoentretenimiento > 4)
+                {
+                    Console.WriteLine("Opcion invalida");
+                }
+
+                Console.WriteLine("Duracion en minutos");
+                duracion = int.Parse(Console.ReadLine()); ;
+
+                Console.WriteLine("Clasificacion");
+                Console.WriteLine("1. Todo publico");
+                Console.WriteLine("2. +13");
+                Console.WriteLine("3. +18");
+                clasifi = Console.ReadLine();
+
+                Console.Write("La hora programada (0-23)");
+                horaprogra = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Nivel de la produccion");
+                Console.WriteLine("1. Bajo");
+                Console.WriteLine("2. Medio");
+                Console.WriteLine("3. Alto");
+                nivelprodu = int.Parse(Console.ReadLine());
+
+                ttevaluados++;
+
+                bool valida = true;
+
+                string razon = "";
+
+                if (clasifi == "+13" && (horaprogra < 6 || horaprogra > 22))
+                {
+                    valida = false;
+
+                    razon = "Horario no valido para +13";
+                }
+                else if (clasifi == "+18" && !(horaprogra >= 22 || horaprogra <= 5))
+                {
+                    valida = false;
+                    razon = "Horaio no valido para +18";
+                }
+                if (valida)
+                {
+                    if (tipoentretenimiento == 1 && (duracion < 60 || duracion > 180))
+                    {
+                        valida = false;
+                        razon = "La duracion es invalida ya que no cumple";
+                    }
+                    else if (tipoentretenimiento == 2 && (duracion < 20 || duracion > 90))
+                    {
+                        valida = false;
+                        razon = "La duracion es invalida ya que no cumple";
+                    }
+                    else if (tipoentretenimiento == 3 && (duracion < 30 || duracion > 120))
+                    {
+                        valida = false;
+                        razon = "La duracion es invalida ay que no cumple";
+                    }
+                    else if (tipoentretenimiento == 4 && (duracion < 30 || duracion > 240))
+                    {
+                        valida = false;
+                        razon = "La duracion es invalida poruqe no cumple";
+                    }
+                }
+            }
+    }
+}
