cbc47d0f9544eb01235d20ecf129ec825b5d14ce
rudycardona4056-ctrl <rudycardona4056@gmail.com>
2026-03-23 13:19:21 -0600

    version 8
    Empece con la el switch dentro de un do while para que el usuario decida cuando salir

---
+++ Proyecto 01.RC/version 8.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main()
+    {
+        do        /*se repite el ciclo hasta que el usuario decida salir*/
+        {
+            opcion = Menu();   /**/
+
+            if (opcion < 1 || opcion > 5)         /*verifica si esta en los numeros permitidos*/
+            {
+                Console.WriteLine("La Opcion es invalida");     /*salida*/
+            }
+
+            switch (opcion)
+            {
+                case 1:
+                    Evaluar();   /**/
+                    break;
+
+                case 2:
+                    Reglas();    /**/
+                    break;
+
+                case 3:
+                    Estadisticas();   /**/
+                    break;
+
+                case 4:
+                    Reiniciar();   /**/
+                    break;
+
+                case 5:  /*si quiere salir del sistema*/
+                    Console.WriteLine("Saliendo..................");
+                    break;
+
+                default:  /*si la opcion es invalida*/
+                    Console.WriteLine("Opcion invalida");
+                    break;
+            }
+
+        } while (opcion != 5);   /*la opcion se repite hasta que elija el numero 5*/
+    }
